957e0a9aa84fe231ddae213824efb8569a4835d5
Kyle Greenidge <kylegreen149@gmail.com>
2026-06-21 21:29:38 -0400

    Create the five KPI's used to analytically disect player performance each season

--- scripts/kpis.py
+++ scripts/kpis.py
@@ -13,3 +13,31 @@ phase_map = pd.read_csv("../DataFrames/nba_contract_phases.csv")
 # Merge dataframes using an Inner Join
 merged_df = pd.merge(phase_map, stats, on=['player', 'season'], how='inner')
 print(merged_df.head())
+
+# Formulate KPI'S
+
+#KPI 1: ESV
+merged_df['efficient_scoring_value'] = merged_df['points'] * merged_df['efg_percentage']
+
+# KPI 2: HF
+# 1e-6 cushion protects against accidental division-by-zero errors
+merged_df['hustle_factor'] = (
+    merged_df['total_rebounds'] + merged_df['steals'] + merged_df['blocks']
+) / (merged_df['minutes_played'] + 1e-6)
+
+# KPI 3: OCR
+# 0.1 cushion added to denominator to prevent division-by-zero errors for players with zero turnovers
+merged_df['offensive_creation_rate'] = merged_df['assists'] / (merged_df['turnovers'] + 0.1)
+
+# KPI 4: AR
+merged_df['availability_rate'] = merged_df['games'] / 82
+
+# KPI 5: OIS (Master KPI)
+merged_df['overall_impact_score'] = (
+    merged_df['efficient_scoring_value'] + 
+    (merged_df['hustle_factor'] * 100) + 
+    (2 * merged_df['offensive_creation_rate']) + 
+    (10 * merged_df['availability_rate'])
+)
+
+print(merged_df.head(50))
