12e3f3d83b86ca6028ff48ac7cfb009bbb0efe24
Kyle Greenidge <kylegreen149@gmail.com>
2026-06-21 20:45:56 -0400

    Read all dataframes, create inner join which removes redundant code and keeps all conditionals

--- scripts/kpis.py
+++ scripts/kpis.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
 
-
 # Read NBA Player Stats/Salaries file in DataFrames folder
-stats_and_salaries = pd.read_csv("../DataFrames/NBA Player Stats and Salaries_2010-2025.csv")
+stats = pd.read_csv("../DataFrames/NBA Player Stats and Salaries_2010-2025.csv")
 
-# Filter to show only szns 2015-2025, & players who avg 12 min/game & 
-    # 40 gms/szn to filter bench noise and injured players who may have skewed stats and salaries.
-stats_and_salaries = stats_and_salaries[
-    (stats_and_salaries['season'] >= 2015) & 
-    (stats_and_salaries['season'] <= 2025) & 
-    (stats_and_salaries["games"] >= 40) &
-    (stats_and_salaries['minutes_played'] >= 12)]
+# print(stats.head())
 
-print(stats_and_salaries.head())
+# Read Contract Phases file in DataFrames folder
+phase_map = pd.read_csv("../DataFrames/nba_contract_phases.csv")
+# print(phase_map.head())
+
+# Merge dataframes using an Inner Join
+merged_df = pd.merge(phase_map, stats, on=['player', 'season'], how='inner')
+print(merged_df.head())
